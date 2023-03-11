77b0bac9bd56f415eac68854b4cdd77b77f0baac
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-11 16:03:01 +0000

    Added function to read tables in AWS database

--- database_utils.py
+++ database_utils.py
@@ -12,3 +12,11 @@ class DatabaseConnector:
         data = self.read_db_creds()
         engine = create_engine("postgresql+psycopg2://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}".format(**data))           
         return engine
+    
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        print(inspector.get_table_names())
+
+# get_tables = DatabaseConnector()
+# get_tables.list_db_tables()
